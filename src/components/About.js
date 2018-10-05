c6b570c8bfaa90a90ae93627d219e1206ecceb68
iykeevans <elochi238@gmail.com>
2018-10-05 10:48:23 +0100

    about component for app

---
+++ src/components/About.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default () => {
+  return (
+    <div className="container">
+			<h4 className="center">About</h4>
+			<p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eos incidunt accusamus saepe! Soluta, dolores officiis commodi quos deleniti nostrum, voluptates non minus a aliquam reiciendis cum dignissimos delectus! Est, error?</p>
+    </div>
+  )
+}
