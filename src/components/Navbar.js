7fff8bf39877320c232de9a30cad2ae66c4adca2
iykeevans <elochi238@gmail.com>
2018-10-05 10:50:28 +0100

    Navigation component for handling app navigation

---
+++ src/components/Navbar.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default () => {
+  return (
+    <nav className="nav-wrapper red darken-3">
+			<div className="container">
+				<a href="#" className="brand-logo">PokeTimes</a>
+				<ul className="right">
+					<li><a href="/">Home</a></li>
+					<li><a href="/about">About</a></li>
+					<li><a href="/contact">Contact</a></li>
+				</ul>
+			</div>
+		</nav>
+  )
+}
