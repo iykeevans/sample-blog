99481c9f69e1db6f062c12da0024b9e2c1a23c8f
iykeevans <elochi238@gmail.com>
2018-10-05 10:57:49 +0100

    imported BrowserRouter and Route from react-router-dom and imported home component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React, { Component } from 'react';
 import Navbar from './components/Navbar';
+import { BrowserRouter, Route } from "react-router-dom";
+import Home from './components/Home';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Navbar />
-      </div>
+      <BrowserRouter>
+        <div className="App">
+          <Navbar />
+          <Route path='/' component={Home} />
+        </div>
+      </BrowserRouter>
     );
   }
 }
