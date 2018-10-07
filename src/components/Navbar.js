dc393dd2f338658541d667e08d34827f5814f315
iykeevans <elochi238@gmail.com>
2018-10-07 13:58:11 +0100

    imported link and navlink from react-router-dom then used it in the navbar component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,16 +1,19 @@
 import React from 'react'
+import { Link, NavLink } from 'react-router-dom'
 
-export default () => {
+const Navbar = () => {
   return (
     <nav className="nav-wrapper red darken-3">
 			<div className="container">
-				<a href="#" className="brand-logo">PokeTimes</a>
+				<Link to="/" className="brand-logo">PokeTimes</Link>
 				<ul className="right">
-					<li><a href="/">Home</a></li>
-					<li><a href="/about">About</a></li>
-					<li><a href="/contact">Contact</a></li>
+					<li><NavLink to="/">Home</NavLink></li>
+					<li><NavLink to="/about">About</NavLink></li>
+					<li><NavLink to="/contact">Contact</NavLink></li>
 				</ul>
 			</div>
 		</nav>
   )
 }
+
+export default (Navbar)
