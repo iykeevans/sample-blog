f7081a7a7022c46db8050baa13e7706cfab34dcb
iykeevans <elochi238@gmail.com>
2018-10-07 13:56:40 +0100

    imported axios then connected to jsonplaceholder api then rendered the data received to the home component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,43 @@
-import React from 'react'
+import React, { Component } from 'react'
+import axios from 'axios'
 
-export default () => {
-  return (
-    <div className="container">
-			<h4 className="center">Home</h4>
-			<p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eos incidunt accusamus saepe! Soluta, dolores officiis commodi quos deleniti nostrum, voluptates non minus a aliquam reiciendis cum dignissimos delectus! Est, error?</p>
-    </div>
-  )
+class Home extends Component {
+	state = {
+		posts: []
+	}
+	componentDidMount = () => {
+		axios.get('https://jsonplaceholder.typicode.com/posts')
+			.then(res => {
+				this.setState({
+					posts: res.data.slice(0, 10)
+				})
+			})
+	}
+	
+	render() {
+		console.log(this.state)
+		const { posts } = this.state;
+		const postList = posts.length ? (
+			posts.map(post => {
+				return (
+					<div className="post card" key={post.id}>
+						<div className="card-content">
+							<span className="card-title">{post.title}</span>
+							<p>{post.body}</p>
+						</div>
+					</div>
+				)
+			})
+		) : (
+			<div className="center">No Posts Yet</div>
+		)
+		return (
+			<div className="container">
+				<h4 className="center">Home</h4>
+				{postList}
+			</div>
+		)
+	}
 }
+
+export default Home
